1c6e2bea8fce56309aec458e40b117dfdebd041b
Lars König <lars.koenig@allianz.de>
2020-10-16 22:09:08 +0200

    basic layout

---
+++ main.py
@@ -0,0 +1,58 @@
+import json 
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
+from datetime import datetime
+
+# CONFIG SECTION #
+STATIC_URL_PATH = '/static'
+SECRET_KEY = 'SOME_SECRET_KEY!'
+
+
+# Init the server
+app = Flask(__name__,  static_url_path=STATIC_URL_PATH)
+app.config['SECRET_KEY'] = SECRET_KEY
+socketio = SocketIO(app, logger=False)
+
+
+
+### STATIC FLASK PART ###
+
+# Das ist die Hauptfunktion die die Seite an sich zurückgibt
+@app.route('/')
+def main():
+    '''
+    Main flask function returning the website
+    Serving a website from a function only makes sense if you actually add some dynamic content to it...
+    We will send the current time.
+    '''
+    
+    now = datetime.now()
+    date_time_str = now.strftime("%m/%d/%Y, %H:%M:%S")
+    return render_template('index.html', time=date_time_str)
+
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    '''
+    Das hier sendet den statischen content wie js bilder, mp4 und so....
+    '''
+    return send_from_directory('static', path)
+
+
+### SOCKET FLASK PART ###
+# Receive a message from the front end HTML
+@socketio.on('client_server_namespace')   
+def message_recieved(data):
+    '''
+    This receaves dynamic content which can then be used for anything else...
+    We are just going to send it back to the client to adjust the value displyed 
+    Using emit will send the Data to all client which are connencted...
+    '''
+    print(data)
+    emit('server_client_namespace', data)    
+
+
+# Actually Start the App
+if __name__ == '__main__':
+    """ Run the app. """    
+    socketio.run(app, port=8000, debug=True)
---
+++ static/main.js
@@ -0,0 +1,36 @@
+// Stuff which runs after the js is fully loaded and ready
+$(document).ready(function() {
+
+    // initial connect to socket
+    var socket = io.connect('http://localhost:8000/', {secure: true});
+    console.log("[+] Connected");
+
+    // connect a button press event to the button1
+    document.getElementById("button1").onclick = function() {
+        console.log("Button1 pressed");
+
+        // Emit a message to the 'client_server_namespace'
+        socket.emit('client_server_namespace', {'button':'1'});
+    }
+
+    // connect a button press event to the button2
+    document.getElementById("button2").onclick = function() {
+        console.log("Button2 pressed");
+
+        socket.emit('client_server_namespace', {'button':'2'});
+    }
+
+    // connect a button press event to the button3
+    document.getElementById("button3").onclick = function() {
+        console.log("Button3 pressed");
+
+        socket.emit('client_server_namespace', {'button':'3'});
+    }
+
+    // Message recieved from server in the 'server_client_namespace
+    // You can have as many namespaces as you whish... 
+    socket.on("server_client_namespace", function(message) {
+    	console.log( message);
+	document.getElementById("value").innerHTML = message.button;
+    });
+});
