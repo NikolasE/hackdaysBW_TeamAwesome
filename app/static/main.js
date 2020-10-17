fff5f3b90f2641960aea7a8146837e50fb6dfddc
Lars König <lars.koenig@allianz.de>
2020-10-17 11:11:57 +0200

    stuff

--- app/static/main.js
+++ app/static/main.js
@@ -1,8 +1,10 @@
 // Stuff which runs after the js is fully loaded and ready
+
+/*
 $(document).ready(function() {
 
     // initial connect to socket
-    var socket = io.connect('http://localhost:8000/', {secure: true});
+    var socket = io.connect('https://localhost:8000/', {secure: true});
     console.log("[+] Connected");
 
     // connect a button press event to the button1
@@ -34,3 +36,31 @@ $(document).ready(function() {
 	document.getElementById("value").innerHTML = message.button;
     });
 });
+*/
+
+//Kompass
+if (window.DeviceOrientationEvent) {
+  // Listen for the deviceorientation event and handle the raw data
+  window.addEventListener('deviceorientation', function(eventData) {
+    var compassdir;
+
+    if(event.webkitCompassHeading) {
+      // Apple works only with this, alpha doesn't work
+      compassdir = event.webkitCompassHeading;
+    }
+    else compassdir = event.alpha;
+
+    document.getElementById("direction").innerHTML = Math.ceil(compassdir);
+
+  });
+}
+
+//rotate
+function rotateDirection(idStr, deg) {
+    var domElemnt = document.getElementById("location");
+    if (domElemnt) {
+        domElemnt.style.transform = "rotate(50deg)";
+        console.log("rotation")
+    }
+}
+rotateDirection("location", 5);
