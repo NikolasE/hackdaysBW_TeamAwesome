a4e8c659e1478e19127947f940ec2e03da10903f
Laurenz <git@laure.nz>
2020-10-17 12:44:47 +0200

    move map svg to module

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from flask_socketio import SocketIO, emit, send
 from datetime import datetime
 from pathlib import Path
 
+from map import build_map
+
 # CONFIG SECTION #
 STATIC_URL_PATH = '/static'
 SECRET_KEY = 'SOME_SECRET_KEY!'
@@ -15,83 +17,8 @@ app = Flask(__name__,  static_url_path=STATIC_URL_PATH)
 app.config['SECRET_KEY'] = SECRET_KEY
 socketio = SocketIO(app, logger=False)
 
-### helper functions ###
 
 
-def build_map():
-    svg_map = """
-<svg id="svg-object" width="455" height="1000" xmlns="http://www.w3.org/2000/svg">
-<rect width="500" height="1000" style="fill:white;stroke-width:3;stroke:rgb(0,0,0)" />
-
-<!-- Kasse, Eingang -->
-<rect x="0" y="900" width="200" height="150" style="fill:black;stroke:black;stroke-width:5;fill-opacity:0.3;stroke-opacity:1" />
-<rect x="250" y="800" width="2500" height="100" style="fill:black;stroke:black;stroke-width:5;fill-opacity:0.3;stroke-opacity:1" />
-<text x="25" y="960" fill="#003278" font-size="2.5em">Eingang</text>
-<text x="280" y="865" fill="#003278" font-size="3em">Kasse</text>
-
-<!-- Ganz Links -->
-<rect x="0" y="450" width="25" height="400" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="0" y="75" width="25" height="325" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- ganz Oben -->
-<rect x="0" y="0" width="450" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Oben 4 -->
-<rect x="425" y="75" width="25" height="325" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Oben 3 -->
-<rect x="300" y="75" width="25" height="150" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="325" y="75" width="25" height="150" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="300" y="300" width="25" height="75" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="325" y="300" width="25" height="75" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="300" y="275" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="300" y="375" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Oben  2-->
-<rect x="200" y="75" width="50" height="175" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="200" y="300" width="50" height="100" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Oben 1 -->
-<rect x="100" y="75" width="25" height="125" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="125" y="75" width="25" height="125" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="100" y="250" width="50" height="125" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="100" y="375" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Unten 4 -->
-<rect x="425" y="450" width="25" height="300" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Unten 3 -->
-<rect x="300" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="325" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="300" y="450" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="300" y="725" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Unten 2 -->
-<rect x="100" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="125" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="100" y="450" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="100" y="725" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Unten 1 -->
-<rect x="200" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="225" y="475" width="25" height="250" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="200" y="450" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="200" y="725" width="50" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-
-<!-- Unten 4 -->
-<rect x="100" y="800" width="100" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-<rect x="100" y="825" width="100" height="25" style="fill:#003278;stroke:black;stroke-width:5;fill-opacity:0.5;stroke-opacity:1" />
-    
-<!-- Location -->
-<polygon points="60,850 20,780 100,780" id="location" style="fill:#ffe300;stroke:#003278;stroke-width:5" />
-<circle cx="60" cy="850" r="20" stroke="#003278" stroke-width="5" fill="#ffe300"  />    
-"""
-
-    svg_end = """</svg>"""
-    svg = svg_map + svg_end
-
-    return svg
-1
 ### STATIC FLASK PART ###
 
 # Das ist die Hauptfunktion die die Seite an sich zurückgibt
@@ -167,6 +94,7 @@ def _get_ssl_context():
     else:
         ssl_context = 'adhoc'
     return ssl_context
+
 
 # Actually Start the App
 if __name__ == '__main__':
