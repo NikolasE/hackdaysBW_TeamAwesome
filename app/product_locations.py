09eb040987b3a081f015d05cb09cf31d7feb202a
Laurenz <git@laure.nz>
2020-10-18 14:12:15 +0200

    update kasse location

--- app/product_locations.py
+++ app/product_locations.py
@@ -5,8 +5,7 @@ product_locations = {
     # if you change this list, make sure to remove the cached distances file
     # the file is /tmp/path_cache.json
 
-    # "_user": (0, 0),
-    "_kasse": (899, 399),
+    "_kasse": (820, 370),
     "0003376": (730, 110),
     "0116393": (200, 110),
     "0007873": (700, 110),
